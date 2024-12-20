267c0ab7f05a0d7f2e57f6c8159ec0ffa81c375a
SahilKarwani <sahil.karwani.0213@gmail.com>
2024-12-20 19:31:05 +0530

    Making changes

--- Console_based_Calculator.c
+++ Console_based_Calculator.c
@@ -1,97 +1,119 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int Max_expression_size = 100;
 
-
-int is_digit(char ch) {
+int isdigit(char ch)
+{
     return ch >= '0' && ch <= '9';
 }
 
-int is_space(char ch) {
+int isspace(char ch)
+{
     return ch == ' ' || ch == '\t' || ch == '\n';
 }
 
-int checkOperator(char ch) {
+int checkOperator(char ch)
+{
     return ch == '*' || ch == '/' || ch == '+' || ch == '-';
 }
 
-int checkPrecedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
+int checkPrecedence(char op)
+{
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
     return 0;
 }
 
-int Operation(int a, int b, char op) {
-    switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return b != 0 ? a / b : 0; 
+int operation(int operand2, int operand1, char operator)
+{
+    switch (operator)
+    {
+    case '+':
+        return operand2 + operand1;
+    case '-':
+        return operand2 - operand1;
+    case '*':
+        return operand2 * operand1;
+    case '/':
+        return operand1 != 0 ? operand2 / operand1 : 0;
     }
     return 0;
 }
 
-
-int solveExpression(const char* input, int* output) {
-    int values[Max_expression_size]; // Stack for numbers
+int solveExpression(const char *input, int *output)
+{
+    int values[Max_expression_size];     // Stack for numbers
     char operators[Max_expression_size]; // Stack for operators
     int valuesTop = -1, operatorsTop = -1;
 
-    for (int i = 0; i < strlen(input); i++) {
-        
-        if (is_space(input[i])) {
+    for (int i = 0; i < strlen(input); i++)
+    {
+
+        if (isspace(input[i]))
+        {
             continue;
         }
 
-        
-        if (is_digit(input[i])) {
+        if (isdigit(input[i]))
+        {
             int value = 0;
-            while (i < strlen(input) && is_digit(input[i])) {
+            while (i < strlen(input) && isdigit(input[i]))
+            {
                 value = value * 10 + (input[i] - '0');
-                i++;    
+                i++;
             }
             values[++valuesTop] = value;
-            i--; 
-        } else if (checkOperator(input[i])) {
-            
-            while (operatorsTop >= 0 && checkPrecedence(operators[operatorsTop]) >= checkPrecedence(input[i])) {
-                int b = values[valuesTop--];
-                int a = values[valuesTop--];
-                char op = operators[operatorsTop--];
+            i--;
+        }
+        else if (checkOperator(input[i]))
+        {
 
-                if (op == '/' && b == 0) {
-                    return 1; 
+            while (operatorsTop >= 0 && checkPrecedence(operators[operatorsTop]) >= checkPrecedence(input[i]))
+            {
+                int operand1 = values[valuesTop--];
+                int operand2 = values[valuesTop--];
+                char operator= operators[operatorsTop--];
+
+                if (operation == '/' && operand1 == 0)
+                {
+                    return 1;
                 }
 
-                values[++valuesTop] = Operation(a, b, op);
+                values[++valuesTop] = operation(operand2, operand1, operator);
             }
-            
+
             operators[++operatorsTop] = input[i];
-        } else {
-            return 2; 
+        }
+        else
+        {
+            return 2;
         }
     }
 
-    
-    while (operatorsTop >= 0) {
+    while (operatorsTop >= 0)
+    {
         int b = values[valuesTop--];
         int a = values[valuesTop--];
         char op = operators[operatorsTop--];
 
-        if (op == '/' && b == 0) {
-            return 1; 
+        if (op == '/' && b == 0)
+        {
+            return 1;
         }
 
         values[++valuesTop] = Operation(a, b, op);
     }
 
     *output = values[valuesTop];
-    return 0; 
+    return 0;
 }
 
-int main(){
+int main()
+{
     char input[Max_expression_size];
     printf("Enter a mathematical expression: ");
     scanf("%[^\n]", input);
@@ -99,13 +121,18 @@ int main(){
     int output;
     int status = solveExpression(input, &output);
 
-    if (status == 0) {
+    if (status == 0)
+    {
         printf("%d\n", output);
-    } else if (status == 1) {
+    }
+    else if (status == 1)
+    {
         printf("Error: Division by zero.\n");
-    } else {
+    }
+    else
+    {
         printf("Error: Invalid expression.\n");
     }
-    
+
     return 0;
 }
