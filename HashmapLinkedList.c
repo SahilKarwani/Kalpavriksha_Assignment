07ae74591ce88b9da69080c036cd704d54b92d19
SahilKarwani <sahil.karwani.0213@gmail.com>
2025-02-07 16:06:01 +0530

    updated hashmap linked list in c

--- HashmapLinkedList.c
+++ HashmapLinkedList.c
@@ -1,157 +1,145 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-#define MAX_SIZE 10
- 
-typedef struct Node{
+
+#define HASHTABLE_SIZE 10
+
+struct node {
     int key;
-    int val;
-    struct Node* next;
-}Node;
- 
-void insertion(Node* hashMap[]){
-    int key;
-    printf("Enter key:");
+    int value;
+    struct node* next;
+}*hashTable[HASHTABLE_SIZE], * newNode, * temp;
+
+void insert() {
+    int value, key;
+    printf("Enter Key: ");
     scanf("%d", &key);
-    int val;
-    printf("Enter val:");
-    scanf("%d", &val);
-    int index=key%(MAX_SIZE);
-    Node* newNode=(Node*)malloc(sizeof(Node));
-    newNode->next=NULL;
-    newNode->key=key;
-    newNode->val=val;
-    if(hashMap[index]==NULL){
-        hashMap[index]=newNode;
-        return;
-    }else{
-        Node* temp=hashMap[index];
-        hashMap[index]=newNode;
-        newNode->next=temp;
+    printf("Enter Value: ");
+    scanf("%d", &value);
+    printf("\n");
+    if (value < 0 || key < 0) {
+        printf("Enter non negative value\n");
+    }
+    int index = key % HASHTABLE_SIZE;
+
+    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+
+    if (hashTable[index] == NULL) {
+        hashTable[index] = newNode;
+    }
+    else {
+        temp = hashTable[index];
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
     }
 }
- 
-void searchValue(Node* hashMap[]){
+
+void search() {
     int key;
+    int value;
+    int found = 0;
     printf("Enter key: ");
-    scanf("%d",&key);
-    int index=key%MAX_SIZE;
-    Node* temp=hashMap[index];
-    if(temp==NULL){
-        printf("Invalid Key\n");
-    }else{
-        int flag=1;
-        while(temp!=NULL){
-            if(temp->key==key){
-                flag=0;
-                printf("Value: %d\n",temp->val);
+    scanf("%d", &key);
+    int index = key % HASHTABLE_SIZE;
+    if (hashTable[index] == NULL) {
+        found = 0;
+    }
+    else {
+        temp = hashTable[index];
+        while (temp != NULL) {
+            if (temp->key == key) {
+                found = 1;
+                value = temp->value;
                 break;
             }
-            temp=temp->next;
-        }
-        if(flag){
-           printf("Invalid Key\n"); 
+            temp = temp->next;
         }
     }
-    return;
+    if (found) {
+        printf("Value: %d ", value);
+    }
+    else {
+        printf("key %d not present\n", key);
+    }
+    printf("\n\n");
 }
- 
-void deleteValue(Node* hashMap[]){
-    int key;
-    printf("Enter key:");
-    scanf("%d",&key);
-    int index=key%MAX_SIZE;
-    Node* temp=hashMap[index];
-    if(temp==NULL){
-       printf("Can't delete, Invalid Key\n");
-       return;
-    }
-    if(temp->next==NULL){ // single node for this index
-        if(temp->key==key){
-        free(temp);
-        printf("Key %d deleted\n",key);
-        hashMap[index]=NULL;
-        return;
-        }else {
-            printf("Can't delete, Invalid Key\n");
-            return;
+
+void display() {
+    printf("Hash Table:\n");
+    for (int i = 0;i < HASHTABLE_SIZE;i++) {
+        if (hashTable[i] == NULL) {
+            continue;
         }
-    }
-    Node* prev=temp;
-    temp=temp->next;
-    while(temp!=NULL){
-        if(temp->key==key){ // delete this key
-            prev->next=temp->next;
-            printf("Key %d deleted\n",key);
-            return;
-        }
-        prev=temp;
-        temp=temp->next;
-    }
-    printf("Can't delete, Invalid Key\n");
-}
- 
-void displayValue(Node* hashMap[]){
-    int flag=1;
-    for(int i=0; i<MAX_SIZE; i++){
-        if(hashMap[i]!=NULL){
-            flag=0;
-            Node* temp=hashMap[i];
-            int flag=1;
-            printf("Index %d:",i);
-            while(temp!=NULL){
-                if(flag){
-                    flag=0;
-                    printf("(%d,%d)", temp->key, temp->val);
-                }else{
-                     printf("->(%d,%d)", temp->key, temp->val);
-                }
-                temp=temp->next;
+        else {
+            int first = 1;
+            printf("Index %d: ", i);
+            temp = hashTable[i];
+            while (temp != NULL) {
+                if (!first)printf(" -> ");
+                printf("( %d,%d )", temp->key, temp->value);
+                first = 0;
+                temp = temp->next;
             }
             printf("\n");
         }
     }
-    if(flag){
-        printf("Empty Hashmap\n");
-    }
+    printf("\n\n");
 }
- 
-void intialize(Node* hashMap[]){
-    for(int i=0; i<MAX_SIZE; i++){
-        hashMap[i]=0;
+
+void delete() {
+    int key;
+    printf("Enter key: ");
+    scanf("%d", &key);
+    int index = key % HASHTABLE_SIZE;
+    struct node* prev = NULL;
+    if (hashTable[index] == NULL) {
+        printf("Key %d not present\n");
     }
-}
- 
-int main() {
-    Node* hashMap[MAX_SIZE];
-    intialize(hashMap);
-    int n;
-    printf("Enter number of operations:");
-    scanf("%d", &n);
-    while(n--){
-       int choice;
-       printf("Enter the choice of operation you want to perform");
-       printf("\n1. Insert\n2. Search\n3. Delete\n4. Display\n5. Exit\n");
-        scanf("%d", &choice);
-        switch(choice){
-            case 1:
-            insertValue(hashMap);
-            break;
-            case 2:
-            searchValue(hashMap);
-            break;
-            case 3:
-            deleteValue(hashMap);
-            break;
-            case 4:
-            displayValue(hashMap);
-            break;
-            case 5:
-            printf("Exit\n");
-            return 0;
-            default:
-            printf("Invalid.\n");
+    else {
+        temp = hashTable[index];
+        while (temp != NULL) {
+            if (temp->key == key) {
+                if (prev == NULL) {
+                    hashTable[index] = temp->next;
+                }
+                else {
+                    prev->next = temp->next;
+                }
+                printf("Key %d deleted. ", key);
+                break;
+            }
+            prev = temp;
+            temp = temp->next;
         }
     }
- 
-    return 0;
+    printf("\n\n");
+}
+
+int main() {
+    int choice, numberOfOperation;
+    scanf("%d", &numberOfOperation);
+
+    while (numberOfOperation) {
+        printf("Enter your Choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+        case 1:insert();
+            break;
+        case 2:search();
+            break;
+        case 3:delete();
+            break;
+        case 4:display();
+            break;
+        case 5:printf("Existing Program.\n\n");
+            return 0;
+        default:printf("Wrong Choice\n");
+            break;
+        }
+        numberOfOperation--;
+    }
 }
